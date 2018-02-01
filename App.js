9cd5aa96462d82498d1d589d77ff79a1a7f44b58
Shawn Cicoria <github@cicoria.com>
2018-02-01 14:00:46 +0200

    updates

--- App.js
+++ App.js
@@ -9,14 +9,22 @@ import {
   View
 } from 'react-native';
 
+import * as Keychain from 'react-native-keychain';
+
 export default class TestSign extends Component<{}> {
   requestCode = 0
+  static _service = 'IXOSTORE22';
 
   constructor(props) {
     super(props);
     this.state = {
-      text: props.content
+      text: props.content,      
+      username: '',
+      password: '',
+      service: TestSign._service,
+      status: 'no key',
     };
+
   }
 
   async onPress() {
@@ -33,11 +41,40 @@ export default class TestSign extends Component<{}> {
       { content: this.state.text });
   }
 
+  loadKey() {
+    Keychain
+      .getGenericPassword(TestSign._service)
+      .then((credentials) => {
+        if (credentials) {
+          console.log(JSON.stringify(credentials));
+          this.setState({ ...credentials, status: 'Credentials loaded!' });
+          return Promise.resolve(true);
+        } else {
+          this.setState({ status: 'No credentials stored.' });
+
+          // Keychain library always returns fales, regardless..  
+          debugger;
+          Keychain.setGenericPassword('DID:xxxx', 'PK....', TestSign._service);
+        }
+      })
+      .then( credentialSet => {
+        console.log('set the credentials %s', credentialSet === true);
+      })
+      .catch((err) => {
+        this.setState({ status: 'Could not load nor create credentials. ' + err });
+      });
+  }
+
+  componentWillMount(){
+    this.loadKey();
+  }
+
   render() {
     if (!this.props.content) {
       return (
         <View style={styles.container}>
-          <Text>Nothing to sign!</Text>
+          <Text>Nothing to sign!!!</Text>
+          <Text>{this.state.status}</Text>
         </View>
       );
     }
