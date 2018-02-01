0637f070b1321122ed4c94ad49cd28b88f2feef0
Cedric Franz <cedric.franz@gmail.com>
2018-02-01 15:00:37 +0200

    Added signing basics

--- App.js
+++ App.js
@@ -8,15 +8,45 @@ import {
   Text,
   View
 } from 'react-native';
+import {Ixo} from "ixo-module";
 
 export default class TestSign extends Component<{}> {
   requestCode = 0
 
+
   constructor(props) {
     super(props);
+    /*
     this.state = {
       text: props.content
     };
+    */
+    //Hardcode in some text to sign
+    this.ixo = new Ixo("https://ixo-node.herokuapp.com");
+    var sovrinDID = this.ixo.cryptoUtil.generateSovrinDID(this.ixo.cryptoUtil.generateMnemonic);
+
+    this.state = {
+      sovrinDID: sovrinDID,
+      text: "{message: 'This is a test'}"
+    };
+  }
+
+  signText() {
+    var signature = this.ixo.cryptoUtil.getDocumentSignature(this.state.sovrinDID.secret.signKey, this.state.sovrinDID.verifyKey, this.state.text);
+    var signDate = (new Date()).toJSON();
+    var response = {
+      content: this.state.text,
+      signature: {
+        type: "Ed25519",
+        created: signDate, 
+        publicKey: "0x" + this.state.sovrinDID.verifyKey,
+        creator: "0x" + this.state.sovrinDID.did,
+        signature: "0x" + signature
+      }
+    };
+
+    console.log(response);
+    return response;
   }
 
   async onPress() {
@@ -25,16 +55,18 @@ export default class TestSign extends Component<{}> {
       return;
     }
 
+    var response = this.signText();
+
     const { ActivityCompletion } = NativeModules;
     
     ActivityCompletion.finish(
       ActivityCompletion.OK,
       "com.ixosign.SIGNED",
-      { content: this.state.text });
+      response);
   }
 
   render() {
-    if (!this.props.content) {
+    if (!this.state.text) {
       return (
         <View style={styles.container}>
           <Text>Nothing to sign!</Text>
