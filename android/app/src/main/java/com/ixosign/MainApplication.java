97be8db62d23ead9da606b56c3fb731a83e15bcc
Eric Rozell <erozell@outlook.com>
2018-02-02 03:24:53 -0500

    Adds modal component for fingerprint and PIN auth. (#3)
    The PIN auth is not yet implemented.

--- android/app/src/main/java/com/ixosign/MainApplication.java
+++ android/app/src/main/java/com/ixosign/MainApplication.java
@@ -3,6 +3,7 @@ package com.ixosign;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.hieuvp.fingerprint.ReactNativeFingerprintScannerPackage;
 import com.bitgo.randombytes.RandomBytesPackage;
 import com.oblador.keychain.KeychainPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new ReactNativeFingerprintScannerPackage(),
           new RandomBytesPackage(),
           new KeychainPackage(),
           new IxoSignReactPackage()
